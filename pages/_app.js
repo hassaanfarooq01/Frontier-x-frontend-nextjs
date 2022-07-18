cde91bf8debe5d7b86f95fc18c70b8dd4533d9ff
hassaanfarooq01 <hassaanfarooq71@gmail.com>
2022-07-18 17:12:54 +0500

    crossOrigin added in _app file

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
         <title>FRONTIER X</title>
         {/* GOOGLE FONTS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap"
           rel="stylesheet"
